c0e9c63e7d0bd789858ad6680a3f17e9d5cd2bc4
mery.acevedo <mery.acevedo@catolica.edu.sv>
2022-08-16 17:26:23 -0600

    php ejercicios practica 16/08

---
+++ prueba02.php
@@ -0,0 +1,20 @@
+<?php
+include "includes/header.php";
+echo "hola mundo";
+echo"</br>";
+echo ("hola mundo");
+echo"</br>";
+print_r("hola mundo");
+echo"</br>";
+var_dump("hola mundo");
+
+
+echo"</br>";
+$nombre="Pepe";
+echo "Hola <b> $nombre </b>, bienvenido a clase";
+echo"</br>";
+echo "hola".$nombre."gusto de verte";
+
+
+ include "includes/footer.php";
+?>
